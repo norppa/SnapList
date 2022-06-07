72e1c1e55f4c8e9b175e8a465dca7dfce4956741
Jari Haavisto <jari.haavisto@buutti.com>
2022-06-07 11:50:49 +0300

    add settings activity

--- app/src/main/java/com/ducksoup/snaplist/MainActivity.kt
+++ app/src/main/java/com/ducksoup/snaplist/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ducksoup.snaplist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,4 +18,26 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_settings -> {
+                navigateTo("settings")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigateTo(target: String) {
+        val targetClass = when (target) {
+            "settings" -> SettingsActivity::class.java
+            else -> error("Invalid navigation target")
+        }
+        val intent = Intent(this, targetClass).apply{
+
+        }
+        startActivity(intent)
+    }
 }
+
---
+++ app/src/main/java/com/ducksoup/snaplist/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.ducksoup.snaplist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
